2a7d8071eed735a3d1f3ccc5d97f689bd2264ea7
chrustny <chrustny@gmail.com>
2019-10-25 08:51:47 +0200

    String table without SOLID

---
+++ src/main/java/com/androidcoder/Main.java
@@ -0,0 +1,11 @@
+package com.androidcoder;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        UsersData users = new UsersData();
+        users.printTable();
+
+    }
+}
